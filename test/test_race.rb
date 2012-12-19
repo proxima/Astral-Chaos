235516177bd7aeeeaf6c4af266e50312e627013d
proxima <proxima@botchris.(none)>
2012-12-19 01:16:39 -0500

    updating unit tests

--- test/test_race.rb
+++ test/test_race.rb
@@ -4,7 +4,22 @@ require 'test/unit'
 
 class TestRaceInheritance < Test::Unit::TestCase
   def test_human_inheritance
+    assert Human < Race
+  end
+
+  def test_human_name_set_properly
     h = Human.new
     assert_equal h.name, :Human
   end
+
+  def test_new_human_has_correct_number_of_stats
+    h = Human.new
+    assert_equal h.stats.size, Race::ALL_STATS.size
+  end
+
+  def test_new_human_has_correct_base_stats
+    h = Human.new
+    h.stats.each_pair {|k,v| assert_equal v, Race::BASE_STAT  }
+  end
 end
+
