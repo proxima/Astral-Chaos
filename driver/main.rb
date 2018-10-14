819fb4cef2fbb81be00ccbf12a967d3610cb15fd
Christopher Lee <f4milyp4nt@gmail.com>
2018-10-14 16:40:44 -0400

    event loop error handler

--- driver/main.rb
+++ driver/main.rb
@@ -9,8 +9,13 @@ EM::run do
   EM::start_server '0.0.0.0', 4000, MudServer
 
   EM::error_handler{ |e|
-    puts "Error raised during event loop: #{e.message}"
+    puts "Error in event loop: #{e.message} #{e.inspect}"
+    e.backtrace.each { |frame| puts "#{frame}" }
   }
+
+  EM::add_periodic_timer(2) do
+    MudServer.broadcast("Teehee\r\n");
+  end
 
   EM::add_periodic_timer(HEARTBEAT_INTERVAL) do
     livings = Mud::Object.values.select { |ob| ob.respond_to? :heartbeat }
